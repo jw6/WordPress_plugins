e9cc873d2b2791abe41898e9fade6cb089078a0e
Jay Wang <jaywang007@yahoo.com>
2016-01-28 12:48:41 -0500

    commit

--- wp-job-listing/wp-job-listing.php
+++ wp-job-listing/wp-job-listing.php
@@ -72,3 +72,28 @@ $args = array(
 }
 
 add_action( 'init', 'dwwp_register_post_type' );
+
+
+function dwwp_register_taxonomy(){
+
+	$singular = 'Location';
+	$plural = 'Locations';
+
+	$labels = array(
+	
+	);
+
+	$args = array(
+		'hierarchical'			=>	true,
+		'labels'				=> $labels,
+		'show_ui'				=> true,
+		'show_admin_column'		=> true,
+		'update_count_callback'	=> '_update_post_term_count',
+		'query_var'				=> true,
+		'rewrite'				=> array( 'slug' => 'location'),
+	);
+		
+}
+
+add_action('inti','dwwp_register_taxonomy')
+
