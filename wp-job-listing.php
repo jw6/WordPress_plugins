c0da88bc2b776c30a59f91d2694803890676d5a8
Jay Wang <jaywang007@yahoo.com>
2016-01-28 11:44:13 -0500

    commit

--- wp-job-listing.php
+++ wp-job-listing.php
@@ -16,7 +16,57 @@ if( ! defined( 'ABSPATH' ) ) {
 
 function dwwp_register_post_type() {
 
-	$args = array( 'public' => true, 'label' => 'Job Listing');
+	$singular = 'Job Listing';
+	$plural = "Job Listings";
+
+	$labels = array(
+		'name' 					=> $plural,
+		'singular_name' 		=> $singular,
+		'add_name' 				=> 'Add New',
+		'add_new_item'			=> 'Add New ' . $singular,
+		'edit'					=> 'Edit',
+		'edit_name'				=> 'Edit' . $singular,
+		'new_item'				=> 'New' . $singular,
+		'view'					=> 'View' . $singular,
+		'view_item'				=> 'Search' . $plural,
+		'search'				=> 'Parent' . $singular,
+		'not_found'				=> 'No' . $plural .' found',
+		'not_found_in_trach'	=> 'No' . $plural .' in Trash' 		
+		);
+
+$args = array(
+		'labels'              	  => $labels,
+	        'public'              => true,
+	        'publicly_queryable'  => true,
+	        'exclude_from_search' => false,
+	        'show_in_nav_menus'   => true,
+	        'show_ui'             => true,
+	        'show_in_menu'        => true,
+	        'show_in_admin_bar'   => true,
+	        'menu_position'       => 10,
+	        'menu_icon'           => 'dashicons-businessman',
+	        'can_export'          => true,
+	        'delete_with_user'    => false,
+	        'hierarchical'        => false,
+	        'has_archive'         => true,
+	        'query_var'           => true,
+	        'capability_type'     => 'page',
+	        'map_meta_cap'        => true,
+	        //'capabilities' => array(),
+
+	        'rewrite'             => array( 
+	        	'slug' => $slug,
+	        	'with_front' => true,
+	        	'pages' => true,
+	        	'feeds' => true,
+	        ),
+	        'supports'            => array( 
+	        	'title', 
+	        	'editor', 
+	        	'author', 
+	        	'custom-fields' 
+	        )
+	);
 
 	register_post_type( 'job', $args);
 }
